08a7df7bc03aff883e5c4cc1f1a8e52b800468f9
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-01-08 08:15:30 +0900

    Title: 합과 평균의 차, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260108/합과 평균의 차/sub-of-average-and-sum.java
+++ 260108/합과 평균의 차/sub-of-average-and-sum.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        Scanner sc = new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
         int a, b, c;
 
         a = sc.nextInt();
