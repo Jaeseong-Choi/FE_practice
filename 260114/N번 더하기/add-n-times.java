5638da2cfbd85a0da5352fb8e1068e23891d6d20
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-01-14 09:44:51 +0900

    Title: N번 더하기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260114/N번 더하기/add-n-times.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, n;
+
+        a = sc.nextInt();
+        n = sc.nextInt();
+
+         for (int i = 1; i <= n; i++) {
+            int newNumber = a + n;
+            a = newNumber;
+            System.out.println(newNumber);
+         }
+    }
+}
