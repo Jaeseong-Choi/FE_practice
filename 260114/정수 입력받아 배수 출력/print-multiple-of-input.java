8ba9395d4f338d7cc734b38f39ae3388e1a1a3c4
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-01-14 09:54:42 +0900

    Title: 정수 입력받아 배수 출력, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260114/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <=5; i++) {
+            int newNumber = n * i; 
+            System.out.print(newNumber + " ");
+        }
+
+    }
+}
