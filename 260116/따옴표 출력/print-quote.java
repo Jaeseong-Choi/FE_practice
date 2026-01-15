8e260798880cd67d18c6b88494305597872f23af
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-01-16 07:10:46 +0900

    Title: 따옴표 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260116/따옴표 출력/print-quote.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Ststem.out.print("He says \"It's a really simple sentence\".);
+    }
+}
