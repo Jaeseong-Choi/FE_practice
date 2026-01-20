a695623a950259f2471cf44ae93ffbbcff639979
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-01-20 19:19:20 +0900

    Title: 배열의 값을 3배로, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 260120/배열의 값을 3배로/triple-the-values-of-the-array.java
+++ 260120/배열의 값을 3배로/triple-the-values-of-the-array.java
@@ -3,19 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
+
+        //입력 함수
         Scanner sc = new Scanner(System.in);
 
+        //3*3 빈 배열 만들기 
         int[][] arr = new int[3][3];
 
+        //각 배열에 값을 넣을 for문 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; i++){
+            for (int j = 0; j < arr.length; j++){
                 arr[i][j] = sc.nextInt();
             }
+
         } 
+        // 입력받은 값 출력하기 
           for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; i++){
-                System.out.println((arr[i][j] * 3) + " ");
+            for (int j = 0; j < arr.length; j++){
+                System.out.print(arr[i][j] * 3 + " ");
             }
+            System.out.println();
         } 
     }
 }
