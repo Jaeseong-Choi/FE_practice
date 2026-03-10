3bb3761fe8b31b391141b208d00f8d75c0631f42
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-03-10 09:35:45 +0900

    Title: A 부터 B 까지, Time: 144ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260310/A 부터 B 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.print(a + " ");
+		while (a < b) {
+			if(a % 2 == 0) {
+				a += 3;
+				System.out.print(a+ " ");
+			}
+			else if (a % 2 == 1) {
+				a *= 2;
+				System.out.print(a+ " ");
+			}
+		}
+        
+    }
+}
